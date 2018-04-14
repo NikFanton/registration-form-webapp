1aa9bae91601e48aed10e286e338f02a6b3b2717
Nikita Guchenko <guchenko.n@gmail.com>
2018-04-14 12:05:54 +0300

    init database script

---
+++ src/main/resources/database/sql/init.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+    id        INT(32) AUTO_INCREMENT PRIMARY KEY,
+    login     VARCHAR(64) NOT NULL ,
+    password  VARCHAR(64) NOT NULL );
+
+INSERT INTO user (login, password) VALUES
+  ('jack123', '12345'),
+  ('mike',    'qwerty'),
+  ('peter',   '123'),
+  ('nikita',  'qwerty123'),
+  ('tom',     'abc555'),
+  ('john',     '111');
+
+SELECT * FROM user;
