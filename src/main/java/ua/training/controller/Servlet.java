f3b7d75efdfe1627ac902ca8ecc0a347cbaffa00
Nikita Guchenko <guchenko.n@gmail.com>
2018-04-14 12:07:09 +0300

    servlet

--- src/main/java/ua/training/controller/Servlet.java
+++ src/main/java/ua/training/controller/Servlet.java
@@ -1,4 +1,47 @@
 package ua.training.controller;
 
-public class Servlet {
+import ua.training.controller.command.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Servlet extends HttpServlet {
+    private Map<String, Command> commands= new HashMap<>();
+
+    @Override
+    public void init() {
+        commands.put("login", new Login());
+        commands.put("registration", new Registration());
+        commands.put("logout", new LogOut());
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String path = request.getRequestURI();
+        System.out.println(path);
+        path = path.replaceAll(".*/api/", "");
+        System.out.println(path);
+        Command command = commands.getOrDefault(path, r -> "/index.jsp");
+        String page = command.execute(request);
+        if (page.contains("redirect:")) {
+            response.sendRedirect(page.replace("redirect:", "/api"));
+        }
+        request.getRequestDispatcher(page).forward(request, response);
+    }
 }
