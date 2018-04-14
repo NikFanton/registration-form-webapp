eb51eaf5498e85918cf9519302c00ca30c324fd6
Nikita Guchenko <guchenko.n@gmail.com>
2018-04-14 12:06:40 +0300

    connection utils

--- src/main/java/ua/training/model/util/ConfigurationUtil.java
+++ src/main/java/ua/training/model/util/ConfigurationUtil.java
@@ -1,4 +1,23 @@
 package ua.training.model.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ConfigurationUtil {
+    private static Properties properties = new Properties();
+    private static InputStream in = null;
+
+    public static String getProperty(String propertyName) {
+        FileInputStream fis = null;
+        try {
+            in = new FileInputStream("src/main/resources/config/config.properties");
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty(propertyName);
+    }
 }
--- src/main/java/ua/training/model/util/ConnectionConstants.java
+++ src/main/java/ua/training/model/util/ConnectionConstants.java
@@ -2,7 +2,7 @@ package ua.training.model.util;
 
 public interface ConnectionConstants {
     String DATABASE_DRIVER = "org.h2.Driver";
-    String DATABASE_URL = "jdbc:h2:~/database/food.mv.db";
+    String DATABASE_URL = "jdbc:h2:~/database/registration-system.mv.db";
     String DATABASE_USER = "sa";
     String DATABASE_PASSWORD = "";
 }
